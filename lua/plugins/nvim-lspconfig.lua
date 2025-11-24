1b123255e6b917d33913f978c7ff6a258a066010
Jesús María Gómez Moreno <zus@gomoreno.me>
2025-11-24 20:30:55 +0100

    fixed lspconfig keymaps

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,44 +1,50 @@
 return {
   "neovim/nvim-lspconfig",
-  keys = {
-    { "K", false },
-    { "gK", false },
-    { "C-K", false },
-    { "<M-n>", false },
-    {
-      "N",
-      function()
-        return vim.lsp.buf.hover()
-      end,
-      desc = "Hover",
-    },
-    {
-      "gN",
-      function()
-        return vim.lsp.buf.signature_help()
-      end,
-      desc = "Signature Help",
-      has = "signatureHelp",
-    },
-    {
-      "<C-n>",
-      function()
-        return vim.lsp.buf.signature_help()
-      end,
-      mode = "i",
-      desc = "Signature Help",
-      has = "signatureHelp",
-    },
-    {
-      "<M-k>",
-      function()
-        Snacks.words.jump(vim.v.count1, true)
-      end,
-      has = "documentHighlight",
-      desc = "Next Reference",
-      cond = function()
-        return Snacks.words.is_enabled()
-      end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "K", false },
+          { "gK", false },
+          { "C-K", false },
+          { "<M-n>", false },
+          {
+            "N",
+            function()
+              return vim.lsp.buf.hover()
+            end,
+            desc = "Hover",
+          },
+          {
+            "gN",
+            function()
+              return vim.lsp.buf.signature_help()
+            end,
+            desc = "Signature Help",
+            has = "signatureHelp",
+          },
+          {
+            "<C-n>",
+            function()
+              return vim.lsp.buf.signature_help()
+            end,
+            mode = "i",
+            desc = "Signature Help",
+            has = "signatureHelp",
+          },
+          {
+            "<M-k>",
+            function()
+              Snacks.words.jump(vim.v.count1, true)
+            end,
+            has = "documentHighlight",
+            desc = "Next Reference",
+            cond = function()
+              return Snacks.words.is_enabled()
+            end,
+          },
+        },
+      },
     },
   },
 }
