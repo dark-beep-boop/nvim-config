46a443be912a20a1f8611c3c422b73e42e638083
Jesús María Gómez Moreno <zus@gomoreno.me>
2026-06-19 17:54:24 +0200

    colorscheme changed to everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
-  -- add gruvbox
-  { "folke/tokyonight.nvim" },
+  -- add everforest
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background="hard",
+      })
+    end,
+  },
 
   -- Configure LazyVim to load tokyonight
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "everforest",
     },
   },
 }
